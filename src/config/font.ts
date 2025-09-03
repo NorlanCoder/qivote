7cd7b85ef14e18f23180ce8ebbf41eff2605b5f3
AxelSalim <adjakidjememiaghe@gmail.com>
2025-09-03 16:13:30 +0100

    Ajout de la police "archivo" et de sa classe correspondante dans le fichier font.ts pour enrichir les ressources typographiques du projet.

--- src/config/font.ts
+++ src/config/font.ts
@@ -1,8 +1,9 @@
-export const fonts = ['inter', 'manrope', 'system'] as const
+export const fonts = ['inter', 'manrope', 'archivo', 'system'] as const
 export type Font = (typeof fonts)[number]
 
 export const fontClasses = {
   inter: 'font-inter',
   manrope: 'font-manrope',
+  archivo: 'font-archivo',
   system: 'font-sans'
 } as const
