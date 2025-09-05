487d3059639c67f300d10b65d1e42bdae5531db9
rosmeamagbegnon <rosmeamagbegnon@gmail.com>
2025-09-05 10:57:30 +0200

    HowItWorks

--- src/components/Advantage.tsx
+++ src/components/Advantage.tsx
@@ -1,23 +1,22 @@
 import Image from "next/image";
 import { Check } from "lucide-react";
+import Img1 from "../../public/images/img1.jpg";
 export default function Advantage() {
   return (
-    <div className="w-full bg-[#F5F3FE] py-14">
-        <div  className="max-w-6xl mx-auto  items-center gap-12 flex flex-col lg:flex-row px-4 lg:px-8">
-            <div className=" ">
-                <h1  className="text-gray-900 text-3xl font-semibold sm:text-4xl ">Pourquoi choisir Qivote?</h1>
-                <div className=" max-w-xl mt-6 md:mt-0">
-                    <h1 className="text-sm lg:text-lg pb-3">Une plateforme pensée pour la simplicité, l'éfficacité ... et le succès de vos évènements</h1>
-                    <ul className="space-y-3 text-xs lg:text-sm">
-                        <li className="flex items-center gap-2"><Check size={16} className="text-[#7976C7]" />Expérience utilisateur fluide</li>
-                        <li className="flex items-center gap-2"><Check size={16} className="text-[#7976C7]" />Outils tout-en-un</li>
-                        <li className="flex items-center gap-2"><Check size={16} className="text-[#7976C7]" />Flexibilité & personnalisation</li>
-                        <li className="flex items-center gap-2"><Check size={16} className="text-[#7976C7]" />Sécurité et transparence</li>
-                        <li className="flex items-center gap-2"><Check size={16} className="text-[#7976C7]" />Support dédié</li>
-                    </ul>
-                </div>
+    <div className="w-full  py-14">
+        <div  className="max-w-6xl mx-auto  items-center gap-6 lg:gap-12 flex flex-col md:flex-row px-4 md:px-8">
+            <div className=" max-w-lg lg:max-w-xl mt-6 md:mt-0 space-y-5">
+                <h1  className=" ">Pourquoi choisir QiVOTE?</h1>
+                <h1 className="text-gray-900 text-3xl font-semibold sm:text-4xl">Une plateforme pensée pour la simplicité, l'éfficacité ... et le succès de vos évènements.</h1>
+                <ul className="space-y-3 text-sm text-gray-700">
+                    <li className="flex items-center gap-2"><Check size={20} className="text-black font-extrabold" />Expérience utilisateur fluide</li>
+                    <li className="flex items-center gap-2"><Check size={20} className="text-black font-extrabold" />Outils tout-en-un</li>
+                    <li className="flex items-center gap-2"><Check size={20} className="text-black font-extrabold" />Flexibilité & personnalisation</li>
+                    <li className="flex items-center gap-2"><Check size={20} className="text-black font-extrabold" />Sécurité et transparence</li>
+                    <li className="flex items-center gap-2"><Check size={20} className="text-black font-extrabold " />Support dédié</li>
+                </ul>
             </div>
-            <Image className="rounded-lg" src="/images/img1.jpg" alt="ChiffreImg" width={470} height={320} />
+            <Image className="rounded-lg" src={Img1} alt="ChiffreImg" width={470} height={320} />
         </div>
     </div>
     
--- src/components/HowItWorks.tsx
+++ src/components/HowItWorks.tsx
@@ -1,29 +1,37 @@
 import Image from "next/image";
+import Courbe from "../../public/images/courbe.svg";
+import Icone1 from "../../public/images/icone1.svg";
+import Icone2 from "../../public/images/icone2.svg";
+import Icone3 from "../../public/images/icone3.svg";
 export default function HowItWorks() {
   return (
-    <div className="py-5 lg:py-16  flex flex-col  items-center justify-center max-w-6xl gap-6 mx-auto ">
-      <div className="relative w-full">
-        <Image className="absolute -z-10 " src="/images/courbe.svg" alt="courbe" fill />
-        <div className="flex justify-between px-0 md:px-5 xl:px-16 ">
-            <div className="flex flex-col items-start">
-                <Image className="pb-4" src="/images/icone1.svg" alt="icone1" width={50} height={50} />
-                <div className="max-w-52 ">
-                    <h1 className="font-bold text-sm lg:text-lg pb-1">Comment ça marche?</h1>
-                    <p className="text-xs lg:text-sm ">Qivote simplifie l'organisation d'évènements en vous offrant une plateforme intuitive pour gérer chaque étape, de la planification à la réalisation.</p>
+    <div className="py-8 lg:py-16  flex flex-col  items-center justify-center max-w-6xl gap-6 mx-auto px-4 md:px-8 ">
+      <div className="text-center max-w-xl lg:max-w-2xl py-4 lg:py-8">
+        <h1 className="text-4xl font-bold text-center text-gray-900 pb-4">Comment ça marche ?</h1>
+        <h1 className="text-xl text-gray-700">Notre plateforme de billeterie en ligne rend la gestion d'évènements simple, sécurisée et accessible à tous.</h1>
+      </div>
+      <div className="relative w-full ">
+        <Image className="absolute -z-10 top-0 md:-top-4 lg:-top-8 xl:-top-10" src={Courbe} alt="courbe"  height={1100} />
+        <div className="flex justify-between px-0  md:px-10 lg:px-20">
+            <div className="flex flex-col  ">
+                <Image className="pb-3" src={Icone1} alt="icone1" width={50} height={50} />
+                <div className="max-w-44 lg:max-w-56 ">
+                    <h1 className="font-bold text-sm  pb-1 text-gray-900">Choississez un évènement, faites entendre votre voix</h1>
+                    <p className="text-xs text-gray-700">Parcourez les nominé.es par catégorie, explorez les prochaines étapes, suivez les participants et votez en toute simplicité.</p>
                 </div>
             </div>
-            <div className="flex flex-col items-start">
-                <Image className="" src="/images/icone2.svg" alt="icone2" width={50} height={50} />
-                <div className="max-w-52 ">
-                    <h1 className="font-bold text-sm lg:text-lg pb-1">Comment ça marche?</h1>
-                    <p className="text-xs lg:text-sm ">Qivote simplifie l'organisation d'évènements en vous offrant une plateforme intuitive pour gérer chaque étape, de la planification à la réalisation.</p>
+            <div className="flex flex-col pt-1 md:pt-7 lg:pt-10 xl:pt-11 ">
+                <Image className="pb-2" src={Icone2} alt="icone2" width={50} height={50} />
+                <div className="max-w-44 lg:max-w-56 ">
+                    <h1 className="font-bold text-sm  pb-1 text-gray-900">Votez pour vos favoris (individuellement ou en masse).</h1>
+                    <p className="text-xs text-gray-700">Notre scanner de billets dédié transforme votre téléphone en l'outil ultime pour des enregistrements rapides sans faille.</p>
                 </div>
             </div>
-            <div className="flex flex-col items-start">
-                <Image className="" src="/images/icone3.svg" alt="icone3" width={50} height={50} />
-                <div className="max-w-52 ">
-                    <h1 className="font-bold text-sm lg:text-lg pb-1">Comment ça marche?</h1>
-                    <p className="text-xs lg:text-sm ">Qivote simplifie l'organisation d'évènements en vous offrant une plateforme intuitive pour gérer chaque étape, de la planification à la réalisation.</p>
+            <div className="flex flex-col pt-1 md:pt-5 lg:pt-7 xl:pt-8">
+                <Image className="pb-2" src={Icone3} alt="icone3" width={50} height={50} />
+                <div className="max-w-44 lg:max-w-56 ">
+                    <h1 className="font-bold text-sm pb-1 text-gray-900">Suivez les résultats en temps réel et partagez!</h1>
+                    <p className="text-xs text-gray-700">Grâce à notre système de paiement fiable, vous et vos clients bénéficiez d'une expérience de paiement universelle.</p>
                 </div>
             </div>
         </div>
